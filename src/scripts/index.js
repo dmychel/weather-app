dc239611746ad5f25aa09905d397d6cc2c352860
dmychel <dmychel@tutanota.com>
2023-09-13 16:56:15 -0400

    created scripts folder

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import '../styles/main.css'
+
+// fetch weatherAPI
+// display weatherAPI data
+    // display weatherAPI 5 day forecast
+    // display in F & C
+    // search current weather for desired location
