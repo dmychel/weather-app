2d8ca47e743344f0f6c99203035023fa8edc0564
dmychel <dmychel@tutanota.com>
2023-09-13 22:17:17 -0400

    Search API
    functions:
    	search(value)
    		- This function takes a arguement (users input) and uses the search api to search/autocomplete a line of text. The search api returns a list of possible matches based on the input. The api defaults to the first item in that list. The functiom then initializes the getForecast and getCurrentWeather functions with the search api result as their arguements.

--- src/scripts/weatherAPI.js
+++ src/scripts/weatherAPI.js
@@ -1,31 +1,39 @@
-import {AUTHENTICATION } from "./authentication";
+import { AUTHENTICATION } from "./authentication";
 import { forecastInfo } from "./forecast";
 import { currentWeatherInfo } from "./currentWeather";
 
 // fetch weatherAPI
 // display weatherAPI data
-    // display weatherAPI 3 day forecast
-    // display in F & C
-    // search current weather for desired location
+// display weatherAPI 3 day forecast
+// display in F & C
+// search current weather for desired location
 
-async function getForecast(){
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${AUTHENTICATION}&q=boston&days=3`, {mode: 'cors'});
 
-    response.json().then(function(response){
+export async function search(value) {
+    const response = await fetch(`https://api.weatherapi.com/v1/search.json?key=${AUTHENTICATION}&q=${value}`)
+
+    response.json().then(function (response) {
+        getForecast(value)
+        getCurrentWeather(value)
+        console.log(response)
+    })
+}
+
+async function getForecast() {
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${AUTHENTICATION}&q=boston&days=3`, { mode: 'cors' });
+
+    response.json().then(function (response) {
         console.log(response)
         forecastInfo(response)
     })
 }
 
-async function getCurrentWeather(){
+async function getCurrentWeather() {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${AUTHENTICATION}&q=boston`)
 
-    response.json().then(function(response){
+    response.json().then(function (response) {
         console.log(response)
         currentWeatherInfo(response)
     })
 }
-
-getForecast()
-getCurrentWeather()
 
