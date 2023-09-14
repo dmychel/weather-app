5709a0458523df81f99f6b55ef6f9d7c6e3d167e
dmychel <dmychel@tutanota.com>
2023-09-13 23:23:21 -0400

    initializes display function with three arguements

--- src/scripts/currentWeather.js
+++ src/scripts/currentWeather.js
@@ -1,13 +1,17 @@
-export function currentWeatherInfo(weather){
-    // get current weather
-    // get current city, state, country
-    // get update
-    let location = {
-        city: weather.location.name,
-        state: weather.location.region,
-        country: weather.location.country
-    };
-    let currentWeather = weather.current;
-    let update = weather.current.last_updated;
-    // console.log(update)
+import { display } from "./dom";
+
+export function currentWeatherInfo(weather) {
+  // get current weather
+  // get current city, state, country
+  // get update
+  let location = {
+    city: weather.location.name,
+    state: weather.location.region,
+    country: weather.location.country,
+  };
+  let currentWeather = weather.current;
+  let update = weather.current.last_updated;
+
+  display.displayCurrentWeather(location, currentWeather, update);
+  // console.log(update)
 }
