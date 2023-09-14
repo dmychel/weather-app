a2c2517708737891c4d2665f09df0c2e1544e0f1
dmychel <dmychel@tutanota.com>
2023-09-14 15:23:35 -0400

    removed import

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -1,8 +1,8 @@
 import { search } from "./weatherAPI";
 
-function locationInput(){
+function locationInput() {
     const form = document.getElementById('locationForm')
-    form.addEventListener('submit', (e) =>{
+    form.addEventListener('submit', (e) => {
         e.preventDefault()
 
         let userInput = document.getElementById('locationInput').value;
