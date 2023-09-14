0688d4c9f15d1c2bef5a7099e6d0eda43532586c
dmychel <dmychel@tutanota.com>
2023-09-14 12:31:24 -0400

    Comments and mionor changes
    - Added comments to all the functions
    - Both functions now set their respective container html to an empty string, this way the user can search more than one result without the screen showing their previous search results.
    - create and append weather and forecast container outside the dynamic dom functions so there is not a million duplicates of the same html elements

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,53 +1,68 @@
 export const display = (() => {
+    // Dom Elements
     const main = document.querySelector("main");
+    const currentWeatherContainer = document.createElement("section");
+    const forecastContainer = document.createElement('section');
+
+    // append
+    main.appendChild(currentWeatherContainer);
+    main.appendChild(forecastContainer);
+
     const displayCurrentWeather = (location, currentWeather, update) => {
-        // console.log(location, currentWeather, update);
-        const currentWeatherContainer = document.createElement("section");
+        // reset html
+        currentWeatherContainer.innerHTML = '';
+
+        // create html elements
         const temperatureEl = document.createElement("div");
         const locationEl = document.createElement("div");
         const updateEl = document.createElement('div');
 
+        // set content
         temperatureEl.textContent = `${currentWeather.feelslike_f}`;
         locationEl.textContent = `${location.city}, ${location.state}, ${location.country}`;
         updateEl.textContent = `${update}`
 
 
-        main.appendChild(currentWeatherContainer);
+        // append
         currentWeatherContainer.appendChild(temperatureEl);
         currentWeatherContainer.appendChild(locationEl);
         currentWeatherContainer.appendChild(updateEl);
 
+        // assign class
         currentWeatherContainer.classList = 'current-weather-container';
         temperatureEl.classList = 'temperature';
         updateEl.classList = 'update'
     };
 
     const displayWeek = week => {
-        const forecastContainer = document.createElement('section');
-
-        main.appendChild(forecastContainer)
-
+        // reset html
+        forecastContainer.innerHTML = '';
         for (let i = 0; i < week.length; i++) {
+            // create html elements
             const card = document.createElement('div');
             const maxTemp = document.createElement('p')
             const minTemp = document.createElement('p')
-            const rain = document.createElement('p')
+            const condition = document.createElement('img')
 
+            // set content
+            condition.src = `${week[i].day.condition.icon}`;
             maxTemp.textContent = `${week[i].day.maxtemp_f}`;
             minTemp.textContent = `${week[i].day.mintemp_f}`;
-            rain.textContent = `${week[i].day.condition.text}`
 
+            // append elements
+            card.appendChild(condition);
             card.appendChild(maxTemp);
             card.appendChild(minTemp);
-            card.appendChild(rain);
             forecastContainer.appendChild(card);
 
+            // assign class
             card.classList = 'card';
+            condition.classList = 'condition-img';
+            maxTemp.classList = 'max-temp';
+            minTemp.classList = 'min-temp';
         }
 
         forecastContainer.classList = 'forecast-container';
-
-        console.log(week)
     };
 
     return { displayCurrentWeather, displayWeek };
