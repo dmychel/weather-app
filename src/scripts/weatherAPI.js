8b92e7c0623f4ca116a9b3ab6ca540ebec4b050f
dmychel <dmychel@tutanota.com>
2023-09-13 20:46:08 -0400

    fetch API
    functions:
    	getForecast()
    		- This function fetchs a 3 day fore cast for boston(location is temporary) then initializes the forecastInfo function with the response as an arguement
    
    	getCurrentWeather()
    		- this function fetchs the current weather for boston (location is temporary) then initializes the currentWeatherInfo functuin with the response as an arugement

--- src/scripts/weatherAPI.js
+++ src/scripts/weatherAPI.js
@@ -1,14 +1,31 @@
-console.log('hello world')
 import {AUTHENTICATION } from "./authentication";
+import { forecastInfo } from "./forecast";
+import { currentWeatherInfo } from "./currentWeather";
 
-const body = document.querySelector('body');
+// fetch weatherAPI
+// display weatherAPI data
+    // display weatherAPI 3 day forecast
+    // display in F & C
+    // search current weather for desired location
 
 async function getForecast(){
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${AUTHENTICATION}&q=boston&days=5`, {mode: 'cors'});
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${AUTHENTICATION}&q=boston&days=3`, {mode: 'cors'});
 
     response.json().then(function(response){
         console.log(response)
+        forecastInfo(response)
+    })
+}
+
+async function getCurrentWeather(){
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${AUTHENTICATION}&q=boston`)
+
+    response.json().then(function(response){
+        console.log(response)
+        currentWeatherInfo(response)
     })
 }
 
 getForecast()
+getCurrentWeather()
+
