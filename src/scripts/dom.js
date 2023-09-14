545e1bdba1b8f9b46443ddea1dfbd4fed304d5ab
dmychel <dmychel@tutanota.com>
2023-09-14 15:21:35 -0400

    Display C
    - The functions are identical to the first two functions. The only difference is they display the celsius temp information from the API

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -59,7 +59,7 @@ export const display = (() => {
             condition.src = `${week[i].day.condition.icon}`;
             maxTemp.textContent = `${week[i].day.maxtemp_f}`;
             minTemp.textContent = `${week[i].day.mintemp_f}`;
-            date.textContent = `${week[i].date}`
+            date.textContent = `${week[i].date}`;
 
 
             // append elements
@@ -79,5 +79,77 @@ export const display = (() => {
         forecastContainer.classList = 'forecast-container';
     };
 
-    return { displayCurrentWeather, displayWeek };
+    // celsius
+    const displayWeek_C = week => {
+        // reset html
+        forecastContainer.innerHTML = '';
+        for (let i = 0; i < week.length; i++) {
+            // create html elements
+            console.log(week[i])
+            const card = document.createElement('div');
+            const maxTemp = document.createElement('p');
+            const minTemp = document.createElement('p');
+            const condition = document.createElement('img');
+            const date = document.createElement('p');
+
+            // set content
+            condition.src = `${week[i].day.condition.icon}`;
+            maxTemp.textContent = `${week[i].day.maxtemp_c}`;
+            minTemp.textContent = `${week[i].day.mintemp_c}`;
+            date.textContent = `${week[i].date}`;
+
+
+            // append elements
+            card.appendChild(condition);
+            card.appendChild(maxTemp);
+            card.appendChild(minTemp);
+            card.appendChild(date)
+            forecastContainer.appendChild(card);
+
+            // assign class
+            card.classList = 'card';
+            condition.classList = 'condition-img';
+            maxTemp.classList = 'max-temp';
+            minTemp.classList = 'min-temp';
+        }
+
+        forecastContainer.classList = 'forecast-container';
+    };
+
+    const displayCurrentWeather_C = (location, weather, update) => {
+        // reset html
+        currentWeatherContainer.innerHTML = '';
+
+        // create html elements
+        const condition = document.createElement('img');
+        const feelsLikeEl = document.createElement("div");
+        const tempEl = document.createElement('p');
+        const locationEl = document.createElement("div");
+        const updateEl = document.createElement('div');
+
+        // set content
+        condition.src = `${weather.condition.icon}`;
+        feelsLikeEl.textContent = `Feels like ${weather.feelslike_c}`;
+        tempEl.textContent = `${weather.temp_c}`;
+        locationEl.textContent = `${location.city}, ${location.state}`;
+        updateEl.textContent = `${update}`;
+
+
+        // append
+        currentWeatherContainer.appendChild(condition);
+        currentWeatherContainer.appendChild(feelsLikeEl);
+        currentWeatherContainer.appendChild(tempEl);
+        currentWeatherContainer.appendChild(locationEl);
+        currentWeatherContainer.appendChild(updateEl);
+
+        // assign class
+        currentWeatherContainer.classList = 'current-weather-container';
+        condition.classList = 'current-condition-img'
+        feelsLikeEl.classList = 'temperature';
+        tempEl.classList = 'current-temp';
+        locationEl.classList = 'current-location';
+        updateEl.classList = 'update';
+    };
+
+    return { displayCurrentWeather, displayWeek, displayWeek_C, displayCurrentWeather_C };
 })();
