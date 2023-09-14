e93e5a55607271fd5c29aa64a19367b968a24fd8
dmychel <dmychel@tutanota.com>
2023-09-14 13:00:59 -0400

    variables
    created a 'week' variable with the value of forecastday array from the api data. Then the dispaly.displayWeek function is run with 'week' as its arguement

--- src/scripts/forecast.js
+++ src/scripts/forecast.js
@@ -1,17 +1,6 @@
-export function forecastInfo(weather){
-    // get location city,state, and country
-    // get 3 day weather forecast
-    let location = {
-        city : weather.location.name,
-        state: weather.location.region,
-        country: weather.location.country
-    };
-    let forecast = weather.forecast;
-    let day = weather.forecast.forecastday;
+import { display } from "./dom";
 
-    for(let i = 0; i < day.length; i++){
-        // console.log(day[i])
-    }
-    // console.log(forecast)
-    // console.log(location)
+export function forecastInfo(weather) {
+    let week = weather.forecast.forecastday;
+    display.displayWeek(week)
 }
