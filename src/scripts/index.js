4e11d59f60d64f641676afa4c1259caba684a7dd
dmychel <dmychel@tutanota.com>
2023-09-13 20:45:53 -0400

    removed comments

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,2 @@
 import '../styles/main.css'
 import './weatherAPI.js'
-
-// fetch weatherAPI
-// display weatherAPI data
-    // display weatherAPI 5 day forecast
-    // display in F & C
-    // search current weather for desired location
