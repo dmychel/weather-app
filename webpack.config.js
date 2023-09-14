7b76644fa4c6dedb16bb1a45f46ecd7facf94141
dmychel <dmychel@tutanota.com>
2023-09-14 15:24:35 -0400

    added loaders and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/scripts/index.js',
-    devServer: {
-
-        static: './dist',
-
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        })
+  mode: "development",
+  entry: "./src/scripts/index.js",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    }
+  },
 };
