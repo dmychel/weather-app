9c2be95bf203ba8599d76bf0210638732aa84718
dmychel <dmychel@tutanota.com>
2023-09-14 15:19:15 -0400

    1 celsius  2  3 - Added an if statement to check if form on the check box is true or >  4 <, if it is true it will be displayed in

--- src/scripts/forecast.js
+++ src/scripts/forecast.js
@@ -1,6 +1,14 @@
 import { display } from "./dom";
 
 export function forecastInfo(weather) {
+    let scale = document.getElementById('scale').checked;
     let week = weather.forecast.forecastday;
-    display.displayWeek(week)
+    if (scale != true) {
+        display.displayWeek(week)
+    }
+    else {
+        display.displayWeek_C(week)
+    }
+
+
 }
