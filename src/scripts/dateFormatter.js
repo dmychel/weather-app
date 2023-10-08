c51e79e7d073bd677174dc61e2412ed06e1a7f66
dmychel <dmychel@tutanota.com>
2023-10-08 19:12:19 -0400

    reformatted current date to use in API fetch

--- src/scripts/dateFormatter.js
+++ src/scripts/dateFormatter.js
@@ -7,3 +7,14 @@ export function formatCurrentDate(date) {
     const formatter = new Intl.DateTimeFormat(undefined, { dateStyle: "medium", timeStyle: "medium" })
     return formatter.format(new Date(date))
 }
+
+export function currentDate() {
+    const formatter = new Intl.DateTimeFormat('en-US', {day: '2-digit'})
+    const date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = formatter.format(date);
+
+    let current = `${year}-${month}-${day}`
+    return current
+}
