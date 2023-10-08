47c8b245c49d9939588fdbbff22a3568ea176e3f
dmychel <dmychel@tutanota.com>
2023-10-08 19:05:46 -0400

    forecast
    I wanted to add the highest and lowest temp so I needed to add an additional paramter for the currentWeatherInfo function.

--- src/scripts/currentWeather.js
+++ src/scripts/currentWeather.js
@@ -1,18 +1,26 @@
 import { display } from "./dom";
 
-export function currentWeatherInfo(weather) {
+export function currentWeatherInfo(weather,forecast) {
   let scale = document.getElementById('scale').checked;
+
+  // location
   let location = {
     city: weather.location.name,
     state: weather.location.region,
   };
+
+  // current weather
   let currentWeather = weather.current;
   let update = weather.current.last_updated;
 
+  // forecast
+  let forecastTemp = forecast.forecast.forecastday[0].day
+
+  // display metric
   if (scale != true) {
-    display.displayCurrentWeather(location, currentWeather, update)
+    display.displayCurrentWeather(location, currentWeather, forecastTemp, update)
   }
   else {
-    display.displayCurrentWeather_C(location, currentWeather, update);
+    display.displayCurrentWeather_C(location, currentWeather, forecastTemp, update);
   }
 };
